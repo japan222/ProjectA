3441ea8fe89930efaaee4a089406c80f82a38706
japan222 <teenidacumpinij@gmail.com>
2025-04-18 16:04:29 +0700

    💡 updated landing page files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('signup-btn').addEventListener('click', function() {
+    alert('สมัครสมาชิกเรียบร้อยแล้ว');
+});
+
+document.getElementById('contact-btn').addEventListener('click', function() {
+    alert('ติดต่อสอบถาม');
+});
