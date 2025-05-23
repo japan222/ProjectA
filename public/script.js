6bc08a6986ac429603b8a168f9590e0363905919
japan222 <teenidacumpinij@gmail.com>
2025-05-23 21:22:37 +0700

    Delete public/script.js

--- public/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('signup-btn').addEventListener('click', function() {
-    alert('สมัครสมาชิกเรียบร้อยแล้ว');
-});
-
-document.getElementById('contact-btn').addEventListener('click', function() {
-    alert('ติดต่อสอบถาม');
-});
